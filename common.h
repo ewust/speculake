5bdd1c51f733850e95ed850b5fa9b2598215e78c
Eric Wustrow <ewust@umich.edu>
2018-02-20 18:04:09 -0700

    common.h

--- common.h
+++ common.h
@@ -3,5 +3,7 @@
 
 extern void __attribute__((section(".fnptr"))) (*fn_ptr)(void);
 void indirect(void *jmp_ptr) __attribute__((section(".indirect")));
+extern void __attribute__((section(".aliasfnptr"))) (*alias_fn_ptr)(void);
+void alias_indirect(void *jmp_ptr) __attribute__((section(".aliasindirect")));
 
 #endif
