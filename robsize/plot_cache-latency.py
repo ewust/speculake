f9463c5145a9865c1ce167919cac3bb9860134c7
Jack Wampler <jack.wampler@colorado.edu>
2018-05-04 12:26:48 -0600

    Figure updates for speculative measurements

--- robsize/plot_cache-latency.py
+++ robsize/plot_cache-latency.py
@@ -5,45 +5,39 @@ import numpy as np
 
 
 def main():
-    data_i5 = []
-    data_xeon = []
-    with open("cache-miss.dat", "r") as f:
+    data_hit = []
+    data_miss = []
+    with open("data/zbuff-cache-miss.dat", "r") as f:
         for line in f:
             if line.isspace(): 
                 continue
-            data_i5.append(int(line.split()[0]))
+            data_miss.append(int(line.split()[0]))
 
-    with open("cache-miss-zbuff.dat", "r") as f:
+    with open("data/zbuff-cache-hits.dat", "r") as f:
         for line in f:
             if line.isspace(): 
                 continue
-            data_xeon.append(int(line.split()[0]))
+            data_hit.append(int(line.split()[0]))
 
     fig = plt.figure()
-    pdf = fig.add_subplot(111)
-    cdf = pdf.twinx()
+    cdf = fig.add_subplot(111)
 
-    pdf.hist(data_i5, bins=1000, label="I5-7200U Density", color='#ff5050')
-    sorted_data_i5 = np.sort(data_i5)
-    yvals=np.arange(len(sorted_data_i5))/float(len(sorted_data_i5)-1)
-    cdf.plot(sorted_data_i5,yvals, 'r-', label="I5-7200U CDF")
+    sorted_data_miss = np.sort(data_miss)
+    yvals=np.arange(len(sorted_data_miss))/float(len(sorted_data_miss)-1)
+    cdf.plot(sorted_data_miss,yvals, 'b-', label="Cache Miss")
 
-    pdf.hist(data_xeon, bins=1000, alpha=0.7, label="E3-1270 V5 Density", color='c')
-    sorted_data_xeon = np.sort(data_xeon)
-    yvals=np.arange(len(sorted_data_xeon))/float(len(sorted_data_xeon)-1)
-    cdf.plot(sorted_data_xeon,yvals, 'b-', label="E3-1270 V5 CDF")
+    sorted_data_hit = np.sort(data_hit)
+    yvals=np.arange(len(sorted_data_hit))/float(len(sorted_data_hit)-1)
+    cdf.plot(sorted_data_hit,yvals, 'r-', label="Cache Hit")
 
-    pdf.set_xlim(20,5*10**4)
     cdf.set_ylim(0,1)
-    pdf.set_yscale('log')
-    pdf.set_xscale('log')
+    cdf.set_xlim([1,2500])
+    cdf.set_xscale('log')
 
-    pdf.set_xlabel('Cache Miss Duration (cycles)')
-    pdf.set_ylabel('Density')
+    cdf.set_xlabel('Request Latency (cycles)')
     cdf.set_ylabel('CDF')
 
-    fig.suptitle("Cache-Miss Latency Distribution", fontsize=16)
-    fig.legend(bbox_to_anchor=(.85, .7),
+    fig.legend(bbox_to_anchor=(.4, .7),
            bbox_transform=plt.gcf().transFigure)
     
     plt.show()
