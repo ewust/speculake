c04743744c61d78114b3b0ca3afbe121b24639e1
Ian Martiny <ian.martiny@colorado.edu>
2018-06-20 12:37:21 -0600

    add libI, adapt helloWorld to use libI

--- emulator/helloWorld.c
+++ emulator/helloWorld.c
@@ -1,6 +1,14 @@
-#include <stdio.h>
+typedef long int ssize_t;
+typedef unsigned long int size_t;
+int make_syscall(void*, void*, void*, void*, void*, void*, void*);
+ssize_t write(unsigned int fd, const char *buf, size_t count);
+
+ssize_t write(unsigned int fd, const char *buf, size_t count){
+   return make_syscall((void *) 0x1, (void *) fd, (void *) buf, (void *) count, (void *) 0, (void *) 0, (void *) 0);
+}
 
 int main(){
-    printf("hello World\n");
+    char *str = "hello World\n";
+    write(1, str, 12);
     return 0;
 }
